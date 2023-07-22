280bf6e0045785cfd94a1360099ee208b0b94254
nukopy <nukopy@gmail.com>
2023-07-22 16:22:21 +0900

    feat: rename tables

--- db/sqlc/schema.sql
+++ db/sqlc/schema.sql
@@ -1,10 +1,10 @@
-create table author (
+create table authors (
     id integer primary key,
     name varchar(99) not null,
     created_at timestamp not null default now()
 );
 
-create table book (
+create table books (
     id integer primary key,
     title varchar(99) not null,
     price integer not null,
@@ -12,5 +12,5 @@ create table book (
     created_at timestamp not null default now()
 );
 
-alter table book
-add foreign key (author_id) references author (id);
+alter table books
+add foreign key (author_id) references authors (id);
